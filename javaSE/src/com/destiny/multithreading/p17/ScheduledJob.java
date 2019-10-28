d741f5d42712cbc9e25bb443363c29dec9747179
journeylater <journeylater@gmail.com>
2019-10-28 12:02:25 +0800

    p17、p18、线程池

---
+++ javaSE/src/com/destiny/multithreading/p17/ScheduledJob.java
@@ -0,0 +1,24 @@
+package com.destiny.multithreading.p17;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
+class Temp extends Thread {
+    public void run() {
+        System.out.println("run");
+    }
+}
+
+public class ScheduledJob {
+	
+    public static void main(String args[]) throws Exception {
+    
+    	Temp command = new Temp();
+        ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+        
+        ScheduledFuture<?> scheduleTask = scheduler.scheduleWithFixedDelay(command, 1, 3, TimeUnit.SECONDS);
+    
+    }
+}
