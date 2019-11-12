375f1b22ac368148db38c6931a42ef2c530c665d
journeylater <journeylater@gmail.com>
2019-11-12 11:31:03 +0800

    网络编程

---
+++ javaSE/src/com/destiny/net/chat/chat04/SxtUtils.java
@@ -0,0 +1,25 @@
+package com.destiny.net.chat.chat04;
+
+import java.io.Closeable;
+
+/**
+ * 工具类
+ * 
+ *
+ */
+public class SxtUtils {
+	/**
+	 * 释放资源
+	 */
+	public static void close(Closeable... targets ) {
+		for(Closeable target:targets) {
+			try {
+				if(null!=target) {
+					target.close();
+				}
+			}catch(Exception e) {
+				
+			}
+		}
+	}
+}
