002238fdb6bce5daf61321d1a2742ae2af12a9ba
journeylater <journeylater@gmail.com>
2019-12-04 12:46:07 +0800

    activemq 消息队列

--- activemq/src/com/destiny/topic/Receiver.java
+++ activemq/src/com/destiny/topic/Receiver.java
@@ -1,4 +1,4 @@
-package com.destiny.helloworld;
+package com.destiny.topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -8,6 +8,9 @@ import javax.jms.*;
 public class Receiver {
 
     public static void main(String[] args) throws JMSException {
+
+        //定义发送消息的队列名称
+        String TOPIC_NAME = "MyTopicMessage";
 
         //1.建立连接工厂
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("bhz", "bhz", "tcp://192.168.229.130:61616");
@@ -19,10 +22,10 @@ public class Receiver {
         Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
 
         //4.创建队列
-        Destination destination = session.createQueue("queue1");
+        Destination destination = session.createTopic(TOPIC_NAME);
 
         //5.创建消费者
-        javax.jms.MessageConsumer consumer = session.createConsumer(destination);
+        MessageConsumer consumer = session.createConsumer(destination);
 
         //6.消费者接收数据
        consumer.setMessageListener(new MessageListener() {
--- activemq/src/com/destiny/topic/Sender.java
+++ activemq/src/com/destiny/topic/Sender.java
@@ -1,4 +1,4 @@
-package com.destiny.helloworld;
+package com.destiny.topic;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
@@ -10,6 +10,9 @@ import javax.jms.*;
 public class Sender {
     public static void main(String[] args) throws JMSException {
 
+        //定义发送消息的主题名称
+        String TOPIC_NAME = "MyTopicMessage";
+
         //1.建立连接工厂
         ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("bhz", "bhz", "tcp://192.168.229.130:61616");
 
@@ -20,8 +23,9 @@ public class Sender {
         //3.创建session会话
        // Session session = connection.createSession(Boolean.FALSE, Session.AUTO_ACKNOWLEDGE);
         Session session = connection.createSession(Boolean.TRUE, Session.AUTO_ACKNOWLEDGE);
-        //4.创建队列
-        Destination destination = session.createQueue("queue1");
+
+        //4.创建目标
+        Destination destination = session.createTopic(TOPIC_NAME);
 
         //5.创建生产者
         MessageProducer producer = session.createProducer(destination);
@@ -34,8 +38,8 @@ public class Sender {
 
         //8.设置消息内容
         for (int i = 0; i < 100; i++) {
-            message.setText("我是消息 ["+i+"] lalalala");
-            System.out.println("[队列模式]生产者生产消息：  "+"我是消息 ["+i+"] lalalala");
+            message.setText("[主题模式]我是消息 ["+i+"] lalalala");
+            System.out.println("生产者生产消息：  "+"我是消息 ["+i+"] lalalala");
             producer.send(message); //发送消息
         }
         session.commit(); //使用事务必须提交
